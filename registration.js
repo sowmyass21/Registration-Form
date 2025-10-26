d35ee249ea59c6dbe3bef1d935e629bad7b7dbd1
sowmya <sowmyashahukari21@gmail.com>
2025-10-26 17:02:02 +0530

    fix localhost

--- registration.js
+++ registration.js
@@ -27,7 +27,7 @@ document.addEventListener("DOMContentLoaded", () => {
       };
 
       try {
-        const response = await fetch("http://localhost:5000/register", {
+        const response = await fetch("http://Registration-Form.onrender.com/register", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify(data)
